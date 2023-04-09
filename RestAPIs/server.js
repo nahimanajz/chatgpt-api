b6577843007e844d94dd4113e1019892b3a171ba
elijahsoft <elijahladdie2004@gmail.com>
2023-04-09 10:32:36 +0200

    summarize a phrase

--- RestAPIs/server.js
+++ RestAPIs/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import env from "dotenv";
 import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai-api'; // used only in summarizing lengthy text
 import fs from 'fs'
 import path from 'path'
 import FormaData from 'form-data'
@@ -15,10 +16,10 @@ const configuration = new Configuration({
   apiKey: `Bearer ${process.env.OPENAIA_API_KEY}`,
 });
 const openai = new OpenAIApi(configuration);
-app.get("/transcribe", async(req, res)=> {
+app.get("/transcribe", async (req, res) => {
 
   const __filename = new URL(import.meta.url).pathname;
-  const __dirname =path.dirname(__filename)
+  const __dirname = path.dirname(__filename)
   const filePath = path.join(__dirname, './audio/EnglishinaMinute_PartyAnimal.mp3')
   const model = "whisper-1";
 
@@ -26,25 +27,64 @@ app.get("/transcribe", async(req, res)=> {
   formRecords.append("model", model)
   formRecords.append("file", fs.createReadStream(filePath))
 
-  return  await axios.post("https://api.openai.com/v1/audio/transcriptions", formRecords, {
+  return await axios.post("https://api.openai.com/v1/audio/transcriptions", formRecords, {
     headers: {
-      Authorization:`Bearer ${process.env.OPEN_SECOND_KEY}`,
-      'Content-Type':"multipart/form-data;boundary=${formRecords._boundary}"
+      Authorization: `Bearer ${process.env.OPEN_SECOND_KEY}`,
+      'Content-Type': "multipart/form-data;boundary=${formRecords._boundary}"
     }
   })
-  .then(resp=> res.send({status: 200, transcribe: resp.data}))
-  .catch(error=> res.status(500).send({status: 500, message: error.message}))
+    .then(resp => res.send({ status: 200, transcribe: resp.data }))
+    .catch(error => res.status(500).send({ status: 500, message: error.message }))
 })
-
-app.get('/test',(req, res)=>{
-  return res.status(200).json({message: "it works"})
-})
-
-app.post("/find-complexity",  async (req, res) => {
+app.post("/summarize", async (req, res) => {
+  const openaiObj = new OpenAI(process.env.OPEN_SECOND_KEY)
+  /*
   try {
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: 
+    const { prompt } = req.body
+    const options = {
+      model: 'text-davinci-002',
+      prompt,
+      temperature: 0.5,
+      maxTokens: 50,
+      n: 1,
+      stop: ['\n']
+
+    }
+    const { choices } = await openaiObj.complete(options)
+    const [{ text }] = choices;
+    return res.status(200).json({ summary: text.trim() })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: error })
+
+  }
+  */
+  const { data } = await openaiObj.complete({
+    engine: 'davinci',
+    prompt: req.body.prompt,
+    maxTokens: 355,
+    temperature: 0.9,
+    topP: 1,
+    presencePenalty: 0,
+    frequencyPenalty: 0,
+    bestOf: 1,
+    n: 1,
+    stream: false,
+    stop: ['\n', "testing"]
+  })
+  const hasText = req.body.prompt.includes(data.choices[0].text)
+  return res.status(200).send({hasText, data})
+})
+
+app.get('/test', (req, res) => {
+  return res.status(200).json({ message: "it works" })
+})
+
+app.post("/find-complexity", async (req, res) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt:
         `
         const example = (arr)=> {
           arr.map((item)=> {
@@ -54,23 +94,23 @@ app.post("/find-complexity",  async (req, res) => {
         the time complexity of this function  is 
         ###
         `,
-        max_tokens: 64,
-        temperature: 0,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty:0.0,
-        stop:["\n"]
+      max_tokens: 64,
+      temperature: 0,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"]
 
-      });
+    });
 
 
-    return  res.status(200).json({
+    return res.status(200).json({
       success: true,
       data: response.data.choices[0].text,
       message: "api post is working",
     });
   } catch (error) {
-    return res.status(500).json({message: error});
+    return res.status(500).json({ message: error });
   }
 });
 
